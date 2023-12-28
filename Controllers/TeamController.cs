81afc9a632919390bba2f6603e728bde1b0bdf6d
Budidhavinaykuamrgoud <vinaygoud911@gmail.com>
2023-12-28 15:32:44 +0530

    Assignment12

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        private readonly ApplicationDbContext _context;
+
+        public TeamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var teams = _context.Teams.ToList();
+            return View(teams);
+        }
+    }
+}
